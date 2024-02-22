6e7815beb356cf2fbf3f373abee68fcd86a29b9f
leo <leopold@lchappuis.fr>
2024-02-22 13:21:11 +0100

    updatedconf

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
+const fs = require('fs');
+
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    https: {
+      key: fs.readFileSync('/etc/letsencrypt/live/sniffle.lchappuis.fr/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/sniffle.lchappuis.fr/fullchain.pem'),
+    },
+    compress: true,
+    port: 3005,
     allowedHosts: [
       'localhost',
       'i-etix.lchappuis.fr',
       'front.lchappuis.fr',
-      'lchappuis.fr'
+      'lchappuis.fr',
+      'sniffle.lchappuis.fr',
     ],
   },
 })
