267db2f29c9581b8cb744def52ff0bac0946cc0a
leo <leopold@lchappuis.fr>
2024-02-22 02:28:57 +0100

    modified colors

--- src/components/Application.vue
+++ src/components/Application.vue
@@ -9,8 +9,11 @@ const myLines = ref([]);
 let colors = ["#20B2AA", "#FAEBD7", "#008B8B", "#BDB76B", "#8FBC8F", "#DAA520", "#CD5C5C", "#66CDAA","#808000","#6B8E23","#FFE4B5","#FFE4E1","#EEE8AA", "#CD853F", "#BC8F8F",
 "#6A5ACD", "#F4A460", "ffbdfd", "bde0ff", "cbbd97", "#E8AABE", "#4AA3A2", "#C8BED1", "#B4F2E5", "#F3D4B0", "#F7AF9D",
 "#B384A7", "#CE8F8A"]
-var UVs = new Map();
+const UVs = ref(new Map());
 var days = new Map();
+
+
+console.log(UVs.value)
 
 days.set("LUNDI...", 1)
 days.set("MARDI...", 2)
@@ -20,6 +23,45 @@ days.set("VENDREDI", 5)
 days.set("SAMEDI..", 6)
 
 
+
+function rerollColors(){
+  colors = shuffleArray(colors);
+  let i =  0;
+  for (const [key] of UVs.value) {
+    UVs.value.set(key, colors[i]);
+    i++;
+  }
+  Cookies.set('UVS', JSON.stringify(Array.from(UVs.value.entries())), {expires : undefined});
+
+}
+
+function applyTheme1(){
+  colors = ["#20B2AA", "#FAEBD7", "#008B8B", "#BDB76B", "#8FBC8F", "#DAA520", "#CD5C5C", "#66CDAA","#808000","#6B8E23","#FFE4B5","#FFE4E1","#EEE8AA", "#CD853F", "#BC8F8F",
+  "#6A5ACD", "#F4A460", "ffbdfd", "bde0ff", "cbbd97", "#E8AABE", "#4AA3A2", "#C8BED1", "#B4F2E5", "#F3D4B0", "#F7AF9D",
+  "#B384A7", "#CE8F8A"]
+  let i =  0;
+  for (const [key] of UVs.value) {
+    UVs.value.set(key, colors[i]);
+    i++;
+  }
+  Cookies.set('UVS', JSON.stringify(Array.from(UVs.value.entries())), {expires : undefined});
+
+}
+
+function applyTheme2(){
+  colors = ["#20B2AA", "#FAEBD7", "#008B8B", "#BDB76B", "#8FBC8F", "#DAA520", "#CD5C5C", "#66CDAA","#808000","#6B8E23","#FFE4B5","#FFE4E1","#EEE8AA", "#CD853F", "#BC8F8F",
+  "#6A5ACD", "#F4A460", "ffbdfd", "bde0ff", "cbbd97", "#E8AABE", "#4AA3A2", "#C8BED1", "#B4F2E5", "#F3D4B0", "#F7AF9D",
+  "#B384A7", "#CE8F8A"]
+  colors = colors.reverse()
+  let i =  0;
+  for (const [key] of UVs.value) {
+    UVs.value.set(key, colors[i]);
+    i++;
+  }
+  Cookies.set('UVS', JSON.stringify(Array.from(UVs.value.entries())), {expires : undefined});
+
+}
+
 function fromStorage(){
   let temp1 = Cookies.get('EDT')
   let temp2 = Cookies.get('FIRTLINE')
@@ -28,11 +70,11 @@ function fromStorage(){
   if(temp1 !== undefined && temp2 !== undefined && temp3 !== undefined){
     text.value = temp3;
     firstLine.value = temp2.split(',');
-    UVs = new Map(JSON.parse(temp3))
-    myLines.value = JSON.parse(temp1)
-    console.log(UVs)
+    UVs.value = new Map(JSON.parse(temp3));
+    myLines.value = JSON.parse(temp1);
   }
 }
+
 
 fromStorage();
 
@@ -40,7 +82,7 @@ function reset() {
   text.value = '';
   firstLine.value = [];
   myLines.value = [];
-  UVs = new Map();
+  UVs.value = new Map();
   colors = ["#20B2AA", "#FAEBD7", "#008B8B", "#BDB76B", "#8FBC8F", "#DAA520", "#CD5C5C", "#66CDAA","#808000","#6B8E23","#FFE4B5","#FFE4E1","#EEE8AA", "#CD853F", "#BC8F8F",
 "#6A5ACD", "#F4A460"];
 }
@@ -66,7 +108,7 @@ function exportToPDF(){
   const opt = {
   margin:       0,
   filename:     'sniffle_UTC_edt.pdf',
-  image:        { type: 'jpeg', quality:   0.999 },
+  image:        { type: 'jpeg', quality:   75 },
   html2canvas:  { scale:   1 },
   jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
 };
@@ -95,7 +137,6 @@ function pushTime(mystring){
 
 function onInput(e) {
   text.value = e.target.value
-  colors = shuffleArray(colors)
   if(text.value ===""){
     myLines.value = [];
     firstLine.value = []
@@ -104,7 +145,7 @@ function onInput(e) {
       filterInputData();
       Cookies.set('FIRTLINE', firstLine.value,{ expires: undefined });
       Cookies.set('EDT', JSON.stringify(myLines.value),{ expires: undefined });
-      Cookies.set('UVS', JSON.stringify(Array.from(UVs.entries())), {expires : undefined});
+      Cookies.set('UVS', JSON.stringify(Array.from(UVs.value.entries())), {expires : undefined});
       console.log(JSON.stringify(myLines.value));
     }catch(error){
       console.log("erreur d'input")
@@ -126,14 +167,14 @@ function filterInputData() {
 
     for(let i=0; i<nbUV; i++){
         firstLine.value.push(filteredData[3+i]);
-        UVs.set(filteredData[3+i], colors[i]);
+        UVs.value.set(filteredData[3+i], colors[i]);
     }
     myLines.value.push(firstLine.value);
 
     const line = ref([]);
     for(let i=2+nbUV+1; i<filteredData.length; i++){
       let verifier = false;
-      if(UVs.has(filteredData[i])){
+      if(UVs.value.has(filteredData[i])){
           verifier = true
       }
       if(verifier){
@@ -216,6 +257,25 @@ function filterInputData() {
 
   
   <input v-if="firstLine.length ==  0" class="input-text" type="text" :value="text" @input="onInput" :placeholder="`Copiez collez votre emploi du temps  ici...`">
+  
+
+  <div class="action-wrapper">
+    <div v-if="myLines.length > 4" class="button2-container">
+      <button class="button" @click="rerollColors">Couleurs aléatoires</button>
+    </div>
+    <div v-if="myLines.length > 4" class="button2-container">
+      <button class="button" @click="applyTheme1">Thème 1</button>
+    </div>
+    <div v-if="myLines.length > 4" class="button2-container">
+      <button class="button" @click="applyTheme2">Thème 2</button>
+    </div>
+    <div v-if="myLines.length > 4" class="button2-container">
+      <button class="button" @click="exportToPDF">Export PDF</button>
+    </div>
+    <div class="button1-container">
+      <button class="button" @click="clearStorage" >Réinitialiser</button>
+    </div>
+  </div>
   <div class="grid-wrapper" id="edt" >
     <div class="grid-container-title-wrapper">
       <div class="grid-container-title" v-if="myLines.length >  0">
@@ -256,27 +316,21 @@ function filterInputData() {
               <div class="grid-separator" :style="{ gridRow: i, gridColumn:  1 }"></div>
             </span>
           
-
-          <div class="grid-item" v-for="(item, index) in myLines" :key="index" :style="{ backgroundColor: UVs.get(item[0]), gridRowStart: item[5][0], gridRowEnd: item[5][1] , gridColumn: days.get(item[2])}">
+            <div class="grid-item"  v-for="(item, index) in myLines" :key="index" :style="{ backgroundColor: UVs.get(item[0]), gridRowStart: item[5][0], gridRowEnd: item[5][1] , gridColumn: days.get(item[2])}">
             <div class="grid-subitem">
               <p>{{item[0]}}<br>{{ item[3] + " - " }}{{item[4] + " - " }} {{ item[1] }}</p>
             </div>
           </div>
+
         </div>
+
 
       </div>
     </div>
     
 
 
-  <div class="action-wrapper">
-    <div v-if="myLines.length > 4" class="button2-container">
-      <button class="button" @click="exportToPDF">Export PDF</button>
-    </div>
-    <div class="button1-container">
-      <button class="button" @click="clearStorage" >Réinitialiser</button>
-    </div>
-  </div>
+
 
 </template>
 
